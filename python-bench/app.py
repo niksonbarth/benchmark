cd29c48c08b45493ffb3d4b0f502bac347615669
Marcelo Lino <marcelo.lino@br.experian.com>
2019-06-07 12:16:58 -0300

    lru cache

--- python-bench/app.py
+++ python-bench/app.py
@@ -1,5 +1,6 @@
 from sanic import Sanic
 from sanic.response import json
+from functools import lru_cache
 app = Sanic()
 
 def fib(n):
@@ -7,9 +8,19 @@ def fib(n):
         return n
     return fib(n - 1) + fib(n - 2)
 
+@lru_cache(maxsize=10)
+def fib2(n):
+    if n <= 1:
+        return n
+    return fib2(n - 1) + fib2(n - 2)
+
 @app.route('/bench')
 async def bench(request):
     return json({'message': fib(30)})
+
+@app.route('/bench2')
+async def bench2(request):
+    return json({'message': fib2(30)})
 
 if __name__ == "__main__":
     app.run('0.0.0.0', 8000, workers=8, access_log=True)
